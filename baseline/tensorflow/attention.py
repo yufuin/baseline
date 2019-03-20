abdc1adf8a569988f767e9cd663a320ea5f1005e
yufuin <issigned@gmail.com>
2019-03-20 06:30:12 +0000

    sinusoid_position_encodingをutilsに分離

--- baseline/tensorflow/attention.py
+++ baseline/tensorflow/attention.py
@@ -1,18 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-def sinusoid_position_encoding(sequence_length:"[]", dim:"[]", dtype=tf.float32, wave=10000) -> "[sequence_length, dim]":
-    half_dim = dim // 2
-    d = tf.cast(tf.range(half_dim), dtype) # [dim//2]
-    div = tf.pow(tf.cast(wave, dtype), d / tf.cast(half_dim, dtype=dtype)) # [dim//2]
-    position = tf.cast(tf.range(sequence_length), dtype) # [seq_len]
-    theta = tf.expand_dims(position, 1) / tf.expand_dims(div, 0) # [seq_len, dim//2]
-    sin = tf.sin(theta)
-    cos = tf.cos(theta)
-    encoded = tf.concat([sin, cos], axis=1) # [seq_len, dim]
-    encoded = tf.identity(encoded, name="sinusoid_position_encoding")
-    return encoded
-
+from baseline.tensorflow.utils import sinusoid_position_encoding
 
 class MultiHeadAttentionBase(tf.keras.layers.Layer):
     """
