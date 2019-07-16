b2da80308c662b856d2bdd09625c11b9c8fb4260
yufuin <issigned@gmail.com>
2019-07-17 02:30:25 +0900

    BasicDictionaryの簡潔化

--- baseline/utils/dictionary.py
+++ baseline/utils/dictionary.py
@@ -1,71 +1,71 @@
 import json
 
 class BasicDictionary:
-    _UNK_REPR = "$$UNK$$"
+    def __init__(self, unk_symbol):
+        self._unk_symbol = unk_symbol
+        self._finalized = False
 
-    def __init__(self, dump=None):
-        if dump is None:
-            self._repr_to_id = dict()
-            self._id_to_repr = list()
+        self.dic = {self.unk_symbol:self.unk_symbol_id}
+        self.reverse_dic = {self.unk_symbol_id:self.unk_symbol}
 
-            self._repr_to_id[self.unk_repr] = 0
-            self._id_to_repr.append(self.unk_repr)
+    @property
+    def size(self):
+        return len(self.dic)
+    def __len__(self):
+        return self.size
+
+    @property
+    def unk_symbol_id(self):
+        return 0
+    @property
+    def unk_symbol(self):
+        return self._unk_symbol
+
+    def add(self, key):
+        assert not self.finalized
+        if type(key) in [list, tuple]:
+            return [self.add(k) for k in key]
         else:
-            self.load(dump=dump)
+            if key in self.dic:
+                return self.dic[key]
+            else:
+                value = len(self.dic)
+                self.dic[key] = value
+                self.reverse_dic[value] = key
+                return value
+    def get(self, key):
+        if type(key) in [list, tuple]:
+            return [self.get(k) for k in key]
+        else:
+            return self.dic.get(key, self.unk_symbol_id)
 
-    @property
-    def unk_repr(self):
-        return self._UNK_REPR
-    @property
-    def unk_id(self):
-        if not hasattr(self, "_unk_id"):
-            self._unk_id = self.to_id(self.unk_repr, allow_new=False)
-        return self._unk_id
+    def encode(self, key, allow_new):
+        if allow_new:
+            return self.add(key)
+        else:
+            return self.get(key)
+    def decode(self, value):
+        if type(value) in [list, tuple]:
+            return [self.decode(v) for v in value]
+        else:
+            return self.reverse_dic[value]
 
     def dump(self):
-        return json.dumps(self._id_to_repr)
-    def load(self, dump):
-        del self._unk_id
-        self._id_to_repr = json.loads(dump)
-        self._repr_to_id = {repr_:id_ for id_,repr_ in enumerate(self._id_to_repr)}
+        return dict(self.dic)
+    def load(self, dic):
+        self.dic = dict(dic)
+        self.reverse_dic = {value:key for key,value in self.dic.items()}
 
-    def to_id(self, target_repr, allow_new):
-        if target_repr in self._repr_to_id:
-            return self._repr_to_id[target_repr]
-        else:
-            if allow_new:
-                new_id = len(self)
-                self._repr_to_id[target_repr] = new_id
-                self._id_to_repr.append(target_repr)
-                return new_id
-            else:
-                return self._repr_to_id[self.unk_repr]
+        self._unk_symbol = self.reverse_dic[self.unk_symbol_id]
+        self._finalized = False
 
-    def to_id_recursive(self, target_reprs, allow_new):
-        if isinstance(target_reprs, list):
-            return [self.to_id_recursive(target_reprs=target_repr, allow_new=allow_new) for target_repr in target_reprs]
-        elif isinstance(target_reprs, dict):
-            return {key:self.to_id_recursive(target_reprs=value, allow_new=allow_new) for key, value in target_reprs.items()}
-        else:
-            return self.to_id(target_repr=target_reprs, allow_new=allow_new)
+    def finalize(self):
+        # this is only for self.add
+        self._finalized = True
+    @property
+    def finalized(self):
+        return self._finalized
 
-    def to_repr(self, target_id):
-        assert target_id < len(self), "out of range index"
-        return self._id_to_repr[target_id]
-
-    def to_repr_recursive(self, target_ids):
-        if isinstance(target_ids, list):
-            return [self.to_repr_recursive(target_ids=target_id) for target_id in target_ids]
-        elif isinstance(target_ids, dict):
-            return {key:self.to_repr_recursive(target_ids=value) for key, value in target_ids.items()}
-        else:
-            return self.to_repr(target_id=target_ids)
-
-    def __len__(self):
-        len_repr_to_id = len(self._repr_to_id)
-        len_id_to_repr = len(self._id_to_repr)
-        assert len_repr_to_id == len_id_to_repr
-        return len_repr_to_id
 
 class BILOUDictionary:
     # for BILOU tagged labels
@@ -74,5 +74,5 @@ class BILOUDictionary:
 
 class MultiDictionary:
     # d = MultiDictionary(words="basic", POSs="basic", "entities"="bilou")
-    # train = d.to_id_recursive(json.load(open("train.json")), allow_new=True)
+    # train = d.encode(json.load(open("train.json")), allow_new=True)
     pass
--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -10,96 +10,104 @@ class DictionaryTestCase(unittest.TestCase):
         pass
 
     def test_initialize(self):
-        dictionary = D.BasicDictionary()
-
-        origin_unk_repr = D.BasicDictionary._UNK_REPR
-        self.assertEqual(origin_unk_repr, dictionary.unk_repr)
+        unk_symbol = "$$UNK$$"
+        dictionary = D.BasicDictionary(unk_symbol=unk_symbol)
 
         self.assertEqual(len(dictionary), 1)
-        self.assertTrue(dictionary.unk_repr in dictionary._repr_to_id)
-        self.assertTrue(dictionary.unk_repr in dictionary._id_to_repr)
 
-        unk_id = dictionary.to_id(dictionary.unk_repr, allow_new=False)
-        self.assertEqual(unk_id, dictionary.unk_id)
-
-        reverted_unk_repr = dictionary.to_repr(unk_id)
-        self.assertEqual(reverted_unk_repr, dictionary.unk_repr)
+        self.assertTrue(unk_symbol in dictionary.dic)
+        self.assertEqual(dictionary.get(unk_symbol), dictionary.unk_symbol_id)
+        self.assertEqual(dictionary.decode(dictionary.unk_symbol_id), unk_symbol)
 
     def test_dump_load(self):
-        dictionary1 = D.BasicDictionary()
-        token1_id = dictionary1.to_id("token1", allow_new=True)
-        token2_id = dictionary1.to_id("token2", allow_new=True)
+        unk_symbol1 = "$$UNK-1$$"
+        dictionary1 = D.BasicDictionary(unk_symbol=unk_symbol1)
+        token1_id = dictionary1.add("token1")
+        token2_id = dictionary1.add("token2")
         self.assertNotEqual(token1_id, token2_id)
-        self.assertNotEqual(dictionary1.unk_id, token1_id)
-        self.assertNotEqual(dictionary1.unk_id, token2_id)
+        self.assertNotEqual(dictionary1.unk_symbol_id, token1_id)
+        self.assertNotEqual(dictionary1.unk_symbol_id, token2_id)
         dump = dictionary1.dump()
 
-        dictionary2 = D.BasicDictionary()
-        none_id = dictionary2.to_id("token1", allow_new=False)
-        self.assertEqual(none_id, dictionary2.unk_id)
-        none_id = dictionary2.to_id("token2", allow_new=False)
-        self.assertEqual(none_id, dictionary2.unk_id)
+        unk_symbol2 = "$$UNK-2$$"
+        dictionary2 = D.BasicDictionary(unk_symbol=unk_symbol2)
+        none_id = dictionary2.get("token1")
+        self.assertEqual(none_id, dictionary2.unk_symbol_id)
+        none_id = dictionary2.get("token2")
+        self.assertEqual(none_id, dictionary2.unk_symbol_id)
 
         dictionary2.load(dump)
-        reload_token1_id = dictionary2.to_id("token1", allow_new=False)
+        reload_token1_id = dictionary2.get("token1")
         self.assertEqual(reload_token1_id, token1_id)
-        reload_token2_id = dictionary2.to_id("token2", allow_new=False)
+        reload_token2_id = dictionary2.get("token2")
         self.assertEqual(reload_token2_id, token2_id)
-        self.assertEqual(dictionary1.unk_id, dictionary2.unk_id)
+        self.assertEqual(dictionary1.unk_symbol_id, dictionary2.unk_symbol_id)
 
-    def test_to_id_and_repr(self):
-        dictionary = D.BasicDictionary()
+    def test_add_get_encode(self):
+        unk_symbol = "$$UNK$$"
 
+        dictionary = D.BasicDictionary(unk_symbol=unk_symbol)
         repr_set1 = ["foo", "bar", 42]
-        ids = [dictionary.to_id(r, allow_new=True) for r in repr_set1]
-        self.assertEqual(ids, [1, 2, 3])
+        add_ids1 = dictionary.add(repr_set1)
+        encode_ids1 = dictionary.encode(repr_set1, allow_new=True)
+        self.assertEqual(add_ids1, encode_ids1)
+        # reverse order
+        dictionary = D.BasicDictionary(unk_symbol=unk_symbol)
+        repr_set1 = ["foo", "bar", 42]
+        encode_ids1 = dictionary.encode(repr_set1, allow_new=True)
+        add_ids1 = dictionary.add(repr_set1)
+        self.assertEqual(add_ids1, encode_ids1)
 
         repr_set2 = ["foo", "foobar"]
-        ids = [dictionary.to_id(r, allow_new=False) for r in repr_set2]
-        self.assertEqual(ids, [1, dictionary.unk_id])
+        encode_ids2 = dictionary.encode(repr_set2, allow_new=False)
+        self.assertTrue("foobar" not in dictionary.dic)
+        self.assertEqual(encode_ids2, [1, dictionary.unk_symbol_id])
+        get_ids2 = dictionary.get(repr_set2)
+        self.assertTrue("foobar" not in dictionary.dic)
+        self.assertEqual(get_ids2, encode_ids2)
+        add_ids2 = dictionary.add(repr_set2)
+        self.assertEqual(add_ids2, [1, 4])
+        get_ids2_2 = dictionary.get(repr_set2)
+        self.assertEqual(add_ids2, get_ids2_2)
+        encode_ids2_2 = dictionary.encode(repr_set2, allow_new=False)
+        self.assertEqual(get_ids2_2, encode_ids2_2)
+        # reverse order
+        dictionary = D.BasicDictionary(unk_symbol=unk_symbol)
+        dictionary.encode(repr_set1, allow_new=True)
+        get_ids2 = dictionary.get(repr_set2)
+        self.assertTrue("foobar" not in dictionary.dic)
+        self.assertEqual(get_ids2, encode_ids2)
+        encode_ids2 = dictionary.encode(repr_set2, allow_new=False)
+        self.assertTrue("foobar" not in dictionary.dic)
+        self.assertEqual(encode_ids2, [1, dictionary.unk_symbol_id])
+        self.assertEqual(get_ids2_2, encode_ids2_2)
 
-        self.assertEqual(dictionary.to_repr(dictionary.to_id("foo", allow_new=False)), "foo")
-        self.assertEqual(dictionary.to_repr(dictionary.to_id("bar", allow_new=False)), "bar")
-        self.assertEqual(dictionary.to_repr(dictionary.to_id("foobar", allow_new=False)), dictionary.unk_repr)
-        self.assertNotEqual(dictionary.to_repr(dictionary.to_id("foobar", allow_new=False)), "foobar")
+    def test_encode_and_decode(self):
+        unk_symbol = "$$UNK$$"
+        dictionary = D.BasicDictionary(unk_symbol=unk_symbol)
 
-    def test_to_id_and_repr_recursive(self):
-        dictionary = D.BasicDictionary()
+        repr_set1 = ["foo", "bar", 42]
+        ids1 = dictionary.encode(repr_set1, allow_new=True)
+        self.assertEqual(ids1, [1, 2, 3])
+        self.assertEqual(dictionary.decode(ids1), repr_set1)
+        repr_set2 = ["foo", "foobar"]
+        ids2 = dictionary.encode(repr_set2, allow_new=False)
+        self.assertEqual(ids2, [1, dictionary.unk_symbol_id])
 
-        reprs = {"foo": ["token1", "token2", {"bar":"token1", "baz":"token1"}, ["token3", "token4"]],
-                 "foobar": {"foofoo": "token5", "barbar": "token3"}}
-        ids = dictionary.to_id_recursive(reprs, allow_new=True)
-
-        token1_id = dictionary.to_id("token1", allow_new=False)
-        self.assertNotEqual(token1_id, dictionary.unk_id)
-        token2_id = dictionary.to_id("token2", allow_new=False)
-        self.assertNotEqual(token2_id, dictionary.unk_id)
-        token3_id = dictionary.to_id("token3", allow_new=False)
-        self.assertNotEqual(token3_id, dictionary.unk_id)
-        token4_id = dictionary.to_id("token4", allow_new=False)
-        self.assertNotEqual(token4_id, dictionary.unk_id)
-        token5_id = dictionary.to_id("token5", allow_new=False)
-        self.assertNotEqual(token5_id, dictionary.unk_id)
-
-        self.assertEqual(len(set([token1_id, token2_id, token3_id, token4_id, token5_id])), 5)
-
-        ideal_ids = {"foo": [token1_id, token2_id, {"bar":token1_id, "baz":token1_id}, [token3_id, token4_id]],
-                     "foobar": {"foofoo": token5_id, "barbar": token3_id}}
-        self.assertEqual(ids, ideal_ids)
-
-        self.assertEqual(reprs, dictionary.to_repr_recursive(ids))
+        self.assertEqual(dictionary.decode(ids1), repr_set1)
+        self.assertEqual(dictionary.decode(ids2), ["foo", dictionary.unk_symbol])
+        self.assertEqual(dictionary.decode(ids1), repr_set1)
 
     def test_len(self):
-        dictionary = D.BasicDictionary()
+        unk_symbol = "$$UNK$$"
+        dictionary = D.BasicDictionary(unk_symbol=unk_symbol)
         self.assertEqual(len(dictionary), 1)
 
         repr_set = ["foo", "bar", 42, "foo"]
-        for r in repr_set:
-            dictionary.to_id(r, allow_new=False)
+        dictionary.encode(repr_set, allow_new=False)
         self.assertEqual(len(dictionary), 1)
 
-        for r in repr_set:
-            dictionary.to_id(r, allow_new=True)
+        dictionary.encode(repr_set, allow_new=True)
         self.assertEqual(len(dictionary), 4)
 
 
