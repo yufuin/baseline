b81852b07e4b1c87372269b728d16951e9a03bcd
yufuin <issigned@gmail.com>
2019-07-19 00:37:09 +0900

    変数名をpadding_valueに変更

--- baseline/utils/padding.py
+++ baseline/utils/padding.py
@@ -10,23 +10,23 @@ def _get_padded_shape(seq, _current_depth=0):
     else:
         return [len(seq)]
 
-def pad(seq, padded_value):
+def pad(seq, padding_value):
     assert type(seq) is list
     shape = get_padded_shape(seq)
-    padded, mask = _pad(seq, padded_value=padded_value, shape=shape)
+    padded, mask = _pad(seq, padding_value=padding_value, shape=shape)
     return padded, mask
-def _pad(seq, padded_value, shape, _current_depth=0):
+def _pad(seq, padding_value, shape, _current_depth=0):
     assert type(seq) is list
     if _current_depth == len(shape) - 1:
-        pad_vec = [padded_value]
+        pad_vec = [padding_value]
         pad_vec = pad_vec * (shape[_current_depth] - len(seq))
         mask = [1] * len(seq) + [0] * len(pad_vec)
         return seq + pad_vec, mask
     else:
-        deep_seqs_and_masks = [_pad(deep_seq, padded_value=padded_value, shape=shape, _current_depth=_current_depth+1) for deep_seq in seq]
+        deep_seqs_and_masks = [_pad(deep_seq, padding_value=padding_value, shape=shape, _current_depth=_current_depth+1) for deep_seq in seq]
         deep_seqs, deep_mask = map(list, list(zip(*deep_seqs_and_masks)))
 
-        pad_vec = [padded_value]
+        pad_vec = [padding_value]
         zero_vec = [0]
         for l in reversed(shape[_current_depth+1:]):
             pad_vec = pad_vec * l
