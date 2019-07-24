ca0b19fab1eb4feddfe40cc60d38309043cdf426
yufuin <issigned@gmail.com>
2019-07-24 16:32:50 +0900

    MultiDictionaryを登録

--- baseline/utils/__init__.py
+++ baseline/utils/__init__.py
@@ -2,6 +2,7 @@ from .padding import pad, get_padded_shape
 del padding
 
 from .dictionary import BasicDictionary
+from .dictionary import MultiDictionary
 del dictionary
 
 from .closed_interval import ClosedInterval
