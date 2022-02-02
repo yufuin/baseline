f575c6dfa399eb706bca5a8bd8b0984e9730856c
yufuin <>
2022-02-02 10:51:35 +0900

    make dynamic_ncols always be True

--- baseline/utils/progress_bar.py
+++ baseline/utils/progress_bar.py
@@ -2,6 +2,9 @@ import tqdm
 import contextlib
 
 def closing_tqdm(*args, **kwargs):
+    kwargs = dict(kwargs)
+    if "dynamic_ncols" not in kwargs:
+        kwargs["dynamic_ncols"] = True
     return contextlib.closing(tqdm.tqdm(*args, **kwargs))
 
 def closing_tqdm_iter(*args, **kwargs):
