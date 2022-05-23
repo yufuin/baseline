c00ee389c89d2bb8d83da8e7fb01978b0799c42c
yufuin <>
2022-05-23 09:08:27 +0900

    返り値を3つに

--- baseline/utils/transformers_utils.py
+++ baseline/utils/transformers_utils.py
@@ -1,9 +1,18 @@
 class __Imports:
     import transformers
+    from typing import Optional, List
 
-def from_pretrained_tokenizer(tokenizer_name_or_path:str, add_lf_tokens:bool=False, trim_offsets:bool=False) -> __Imports.transformers.PreTrainedTokenizer:
-    num_added_tokens = 0
-    tokenizer = __Imports.transformers.AutoTokenizer.from_pretrained(tokenizer_name_or_path, trim_offsets=trim_offsets)
+LF_TOKENS = ("\n","\r","\t")
+
+def from_pretrained_tokenizer(tokenizer_name_or_path:str, add_lf_tokens:bool=True, additional_special_tokens:__Imports.Optional[__Imports.List[str]]=None, trim_offsets:bool=False) -> __Imports.transformers.PreTrainedTokenizer:
+    added_special_tokens = list()
     if add_lf_tokens:
-        num_added_tokens += tokenizer.add_special_tokens({"additional_special_tokens":["\n","\r","\t"]})
-    return tokenizer, num_added_tokens
+        added_special_tokens.extend(list(LF_TOKENS))
+    if additional_special_tokens is not None:
+        added_special_tokens.extend(list(additional_special_tokens))
+    tokenizer = __Imports.transformers.AutoTokenizer.from_pretrained(tokenizer_name_or_path, trim_offsets=trim_offsets)
+    if len(added_special_tokens) > 0:
+        num_added_tokens = tokenizer.add_special_tokens({"additional_special_tokens":added_special_tokens})
+    else:
+        num_added_tokens = 0
+    return tokenizer, num_added_tokens, added_special_tokens
